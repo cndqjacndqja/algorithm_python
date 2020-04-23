d5b75b5a700aca66ecd8a1c6e58dabc0e9c65f05
Choong <cndqjacndqja@naver.com>
2020-04-23 20:50:54 +0900

    8주차_1.sequential_search

---
+++ 8_1._sequential_search.py
@@ -0,0 +1,17 @@
+def sequential_seasrch(seq, n):
+    for item in seq:
+        if item == n: return True
+    return False
+
+
+def test_sequential_search():
+    seq = [1, 5, 6, 8, 3]
+    n1 = 5
+    n2 = 7
+    assert (sequential_seasrch(seq, n1) is True)
+    assert (sequential_seasrch(seq, n2) is False)
+    print("테스트 통과!")
+
+
+if __name__ == "__main__":
+    test_sequential_search()
