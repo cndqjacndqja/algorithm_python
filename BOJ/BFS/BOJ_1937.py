747fb32c9f4f7e3f70b45a8d1959776e1a725fcf
Choong <cndqjacndqja@naver.com>
2021-01-05 16:39:25 +0900

    BOJ_1937 아직 해결 못함..

--- BOJ/BFS/BOJ_1937.py
+++ BOJ/BFS/BOJ_1937.py
@@ -7,6 +7,8 @@ dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 visited = [[False] * n for _ in range(n)]
 
 result = 0
+
+
 def dfs(graph, x, y, count):
     visited[x][y] = True
     global result
@@ -14,5 +16,16 @@ def dfs(graph, x, y, count):
 
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == False:
-            
+        if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == False and graph[nx][ny] > graph[x][y]:
+            print(x, y, result)
+            visited[nx][ny] = True
+            dfs(graph, nx, ny, count + 1)
+            visited[nx][ny] = False
+
+
+for i in range(n):
+    for j in range(n):
+        dfs(graph, i, j, 1)
+        visited = [[False] * n for _ in range(n)]
+
+print(result)
