071352b70f9bc6a145bec10d4669a4ff2f21b1ef
Choong <cndqjacndqja@naver.com>
2021-01-07 20:33:12 +0900

    BOJ_1937 다시 풀기

--- BOJ/BFS/BOJ_1937.py
+++ BOJ/BFS/BOJ_1937.py
@@ -4,28 +4,25 @@ graph = [list(map(int, input().split())) for _ in range(n)]
 
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
-visited = [[False] * n for _ in range(n)]
+dp_visited = [[0] * n for _ in range(n)]
 
 result = 0
 
 
-def dfs(graph, x, y, count):
-    visited[x][y] = True
-    global result
-    result = max(result, count)
+def dfs(graph, x, y):
+    if dp_visited[x][y]:
+        return dp_visited[x][y]
 
+    dp_visited[x][y] = 1
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == False and graph[nx][ny] > graph[x][y]:
-            print(x, y, result)
-            visited[nx][ny] = True
-            dfs(graph, nx, ny, count + 1)
-            visited[nx][ny] = False
+        if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] > graph[x][y]:
+            dp_visited[x][y] = max(dp_visited[x][y], dfs(graph, nx, ny) + 1)
+    return dp_visited[x][y]
 
 
 for i in range(n):
     for j in range(n):
-        dfs(graph, i, j, 1)
-        visited = [[False] * n for _ in range(n)]
+        result = max(result, dfs(graph, i, j))
 
 print(result)
