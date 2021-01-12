0b7828122757ec7d998a81f06c0c2a2092f75ed8
Choong <cndqjacndqja@naver.com>
2021-01-12 14:19:35 +0900

    BOJ_2156

---
+++ BOJ/DP/BOJ_2156.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+data = [0]
+for i in range(n):
+    data.append(int(input()))
+
+dp = [0]
+
+dp.append(data[1])
+if n > 1:
+    dp.append(data[1] + data[2])
+
+
+for i in range(3, n + 1):
+        dp.append(max(dp[i - 3] + data[i] + data[i-1], dp[i - 2] + data[i], dp[i - 1]))
+
+print(dp[n])
