65b69ba2407fcb9d39bd39ce65605803e800e830
Choong <cndqjacndqja@naver.com>
2021-01-20 15:47:33 +0900

    BOJ_1699

---
+++ BOJ/DP/BOJ_1699.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+dp = [0] * (n + 1)
+dp[1] = 1
+for i in range(2, n + 1):
+    dp[i] = i
+    for j in range(i):
+        if j * j > i:
+            break
+        dp[i] = min(dp[i], dp[i - j * j] + 1)
+
+print(dp[n])
