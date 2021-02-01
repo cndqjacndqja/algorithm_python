74e11cf18abf25e5cc5565a819f82d29f63a7479
Choong <cndqjacndqja@naver.com>
2021-02-01 21:02:23 +0900

    BOJ_2573

--- BOJ/BFS/BOJ_2573.py
+++ BOJ/BFS/BOJ_2573.py
@@ -5,54 +5,59 @@ n, m = map(int, input().split())
 
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
-
-# 빙산 체크 bfs
-def bfs(data, x, y):
-    q = deque()
-    q.append((x, y))
-    data[x][y] = 0
-
-    while q:
-        Px, Py = q.popleft()
-        for i in range(4):
-            nx, ny = Px + dx[i], Py + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and data[nx][ny] != 0:
-                q.append((nx, ny))
-                data[nx][ny] = 0
+data = [list(map(int, input().split())) for _ in range(n)]
 
 
-def melting(graph):
-    graph_melting = copy.deepcopy(graph)
+# 얼음아 녹아라.
+def melting():
+    data_copy = copy.deepcopy(data)
+
     for i in range(n):
         for j in range(m):
-            if graph[i][j] != 0:
-                count = 0
+            if data_copy[i][j] != 0:
                 for z in range(4):
                     nx, ny = i + dx[z], j + dy[z]
-                    if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-                        count += 1
-                graph_melting[i][j] -= count
-    return graph_melting
+                    if 0 <= nx < n and 0 <= ny < m and data_copy[nx][ny] == 0:
+                        if data[i][j] > 0:
+                            data[i][j] -= 1
 
 
-graph = [list(map(int, input().split())) for _ in range(n)]
-
-result = 0
-
-while True:
-    graph_copy = copy.deepcopy(graph)
-    count = 0
+# 빙산 분리 갯수
+def bfs():
+    data_copy = copy.deepcopy(data)
+    cnt = 0
 
     for i in range(n):
         for j in range(m):
-            if graph_copy[i][j] != 0:
-                bfs(graph_copy, i, j)
-                count += 1
-                break
+            if data_copy[i][j] != 0:
+                q = deque()
+                q.append((i, j))
+                cnt += 1
 
-    if count == 2:
-        print(result)
+                while q:
+                    x, y = q.popleft()
+
+                    for z in range(4):
+                        nx, ny = x + dx[z], y + dy[z]
+                        if 0 <= nx < n and 0 <= ny < m and data_copy[nx][ny] != 0:
+                            data_copy[nx][ny] = 0
+                            q.append((nx, ny))
+
+    return cnt
+
+
+year = 0
+
+while True:
+    count = bfs()
+
+    if count == 0:
+        print(0)
         break
 
-    graph = melting(graph)
-    result += 1
+    if count >= 2:
+        print(year)
+        break
+
+    melting()
+    year+=1
