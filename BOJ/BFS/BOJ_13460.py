f7fb84373b93fb5e7e16f0a0564b4a6841434bbd
Choong <cndqjacndqja@naver.com>
2021-02-20 14:52:45 +0900

    BOJ_13460

--- BOJ/BFS/BOJ_13460.py
+++ BOJ/BFS/BOJ_13460.py
@@ -3,22 +3,58 @@ from _collections import deque
 n, m = map(int, input().split())
 
 data = [list(input()) for _ in range(n)]
-visited = [[0 for _ in range(m)] for _ in range(n)]
+visited = [[[[False for _ in range(m)] for _ in range(n)] for _ in range(m)] for _ in range(n)]
 
+queue = []
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
 
-def bfs(rx, ry, bx, by):
-    q = deque()
-    q.append((rx, ry, bx, by))
+def pos_init():
+    rx, ry, bx, by = 0, 0, 0, 0
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 'R':
+                rx, ry = i, j
+            elif data[i][j] == 'B':
+                bx, by = i, j
+    queue.append((rx, ry, bx, by, 1))
+    visited[rx][ry][bx][by] = True
+
+
+def move(x, y, pram_dx, param_dy):
     cnt = 0
+    while data[x + pram_dx][y + param_dy] != '#' and data[x][y] != 'O':
+        x += pram_dx
+        y += param_dy
+        cnt += 1
+    return x, y, cnt
 
-    while q:
-        rqx, rqy, bqx, bqy = q.popleft()
 
+def solve():
+    pos_init()
+    while queue:
+        rx, ry, bx, by, depth = queue.pop(0)
+        if depth > 10:
+            break
         for i in range(4):
-            nrx, nry, nbx, nby = rqx + dx[i], rqy + dy[i], bqx + dx[i], bqy + dy[i]
-            if 0 < nrx < m - 1 and 0 < nry < m - 1 and 0 < nbx < m - 1 and 0 < nby < m - 1:
-                if visited[nrx][nry] == 0 and data[nrx][nry] == ".":
-                    q.append((nrx, nry, nbx, nby))
-                    
+            nrx, nry, rcnt = move(rx, ry, dx[i], dy[i])
+            nbx, nby, bcnt = move(bx, by, dx[i], dy[i])
+            if data[nbx][nby] != 'O':
+                if data[nrx][nry] == 'O':
+                    print(depth)
+                    return
+                if nrx == nbx and nry == nby:
+                    if rcnt > bcnt:
+                        nrx -= dx[i]
+                        nry -= dy[i]
+                    else:
+                        nbx -= dx[i]
+                        nby -= dy[i]
+                if not visited[nrx][nry][nbx][nby]:
+                    visited[nrx][nry][nbx][nby] = True
+                    queue.append((nrx, nry, nbx, nby, depth + 1))
+
+    print(-1)
+
+
+solve()
--- test/test.py
+++ test/test.py
@@ -1,8 +1,60 @@
+from _collections import deque
+
 n, m = map(int, input().split())
 
-dp = [0 for _ in range(1001)]
-dp[1] = 1
-dp[2] = 3
-for i in range(3, n + 1):
-    dp[i] = dp[i - 1] + dp[i - 2] * 2
-print(dp[n] % m)
+data = [list(input()) for _ in range(n)]
+visited = [[[[False for _ in range(m)] for _ in range(n)] for _ in range(m)] for _ in range(n)]
+
+queue = []
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+
+def pos_init():
+    rx, ry, bx, by = 0, 0, 0, 0
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 'R':
+                rx, ry = i, j
+            elif data[i][j] == 'B':
+                bx, by = i, j
+    queue.append((rx, ry, bx, by, 1))
+    visited[rx][ry][bx][by] = True
+
+
+def move(x, y, pram_dx, param_dy):
+    cnt = 0
+    while data[x + pram_dx][y + param_dy] != '#' and data[x][y] != 'O':
+        x += pram_dx
+        y += param_dy
+        cnt += 1
+    return x, y, cnt
+
+
+def solve():
+    pos_init()
+    while queue:
+        rx, ry, bx, by, depth = queue.pop(0)
+        if depth > 10:
+            break
+        for i in range(4):
+            nrx, nry, rcnt = move(rx, ry, dx[i], dy[i])
+            nbx, nby, bcnt = move(bx, by, dx[i], dy[i])
+            if data[nbx][nby] != 'O':
+                if data[nrx][nry] == 'O':
+                    print(depth)
+                    return
+                if nrx == nbx and nry == nby:
+                    if rcnt > bcnt:
+                        nrx -= dx[i]
+                        nry -= dy[i]
+                    else:
+                        nbx -= dx[i]
+                        nby -= dy[i]
+                if not visited[nrx][nry][nbx][nby]:
+                    visited[nrx][nry][nbx][nby] = True
+                    queue.append((nrx, nry, nbx, nby, depth + 1))
+
+    print(-1)
+
+
+solve()
