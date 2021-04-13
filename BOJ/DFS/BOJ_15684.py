8d4040af2d67cf5ef9c7e9585bbbb057b02b8020
cndqjacndqja <cndqjacndqja@gmail.com>
2021-04-13 11:57:57 +0900

    BOJ_15684

--- BOJ/DFS/BOJ_15684.py
+++ BOJ/DFS/BOJ_15684.py
@@ -1,11 +1,56 @@
 n, m, h = map(int, input().split())
-data = [[0 for _ in range(h+1)] for _ in range(n+1)]
+data = [[0 for _ in range(h + 1)] for _ in range(n + 1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
     data[b][a] = 1
 
 
+def move():
+    for i in range(1, n + 1):
+        x = i
+        for j in range(1, h + 1):
+            if data[x][j] == 1:
+                if x + 1 <= n:
+                    x += 1
+            elif 0 < x - 1 <= n and data[x - 1][j] == 1:
+                x -= 1
+
+        if x != i:
+            return 0
+    return 1
+
+max_size = int(1e9)
+result = max_size
 
 
+def dfs(cnt, idx, r):
+    global result
+    if cnt == r:
+        if move():
+            result = min(cnt, result)
+            return 1
+        else:
+            return 0
 
+    for i in range(idx, n):
+        for j in range(1, h + 1):
+            if data[i][j] == 1:
+                continue
+            elif 0 < i - 1 <= n and data[i - 1][j] == 1:
+                continue
+            data[i][j] = 1
+            dfs(cnt + 1, i, r)
+            data[i][j] = 0
+
+
+def solved():
+    for i in range(4):
+        dfs(0, 1, i)
+
+    if result == max_size:
+        print(-1)
+    else:
+        print(result)
+
+solved()
--- test/test.py
+++ test/test.py
@@ -1,33 +1,3 @@
-n = int(input())
-data = list(map(int, input().split()))
-dx = list(map(int, input().split()))
-
-result = []
-
-
-def dfs(cnt, param_data):
-    if cnt == n:
-        result.append(param_data)
-        return
-
-    if dx[0] > 0:
-        dx[0] -= 1
-        dfs(cnt + 1, param_data + data[cnt])
-        dx[0] += 1
-    if dx[1] > 0:
-        dx[1] -= 1
-        dfs(cnt + 1, param_data - data[cnt])
-        dx[1] += 1
-    if dx[2] > 0:
-        dx[2] -= 1
-        dfs(cnt + 1, param_data * data[cnt])
-        dx[2] += 1
-    if dx[3] > 0:
-        dx[3] -= 1
-        dfs(cnt + 1, int(param_data / data[cnt]))
-        dx[3] += 1
-
-
-dfs(1, data[0])
-print(max(result))
-print(min(result))
+a = 0
+if a:
+    print("test")
