840025c5690751be7183891e4f7b19df4177d8c6
cndqjacndqja <cndqjacndqja@gmail.com>
2021-04-16 11:51:10 +0900

    BOJ_16437

--- BOJ/DFS/BOJ_16437.py
+++ BOJ/DFS/BOJ_16437.py
@@ -1,13 +1,35 @@
 n = int(input())
-amount_data = [[] for _ in range(n)]
-wolf_data = [[] for _ in range(n)]
+tree = [[] for _ in range(n+1)]
+wolf = ['' for _ in range(n+1)]
 
-for _ in range(n-1):
-    ch, per, location = input().split()
-    per = int(per)
-    location = int(location)
-    if ch == 'S':
-        amount_data[location].append()
+for i in range(2, n+1):
+    ch, per, loc = input().split()
+    tree[i].append(int(per))
+    tree[i].append(int(loc))
+    wolf[i] = ch
 
+num = 0
+def dfs(node, idx):
+    global num
+    if idx == 1:
+        num = node[0]
+        return
 
+    if wolf[idx] == 'W':
+        node[0] -= tree[idx][0]
+        if node[0] <= 0:
+            return 0
 
+    dfs(node, tree[idx][1])
+
+def solved():
+    result = 0
+    global num
+    for i in range(2, n+1):
+        if wolf[i] != 'W':
+            num = 0
+            dfs(tree[i], i)
+            result+=num
+    print(result)
+
+solved()
