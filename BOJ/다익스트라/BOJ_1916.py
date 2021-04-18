e8c152f59b6967af73a8cf86d8bd99be1bf9b084
cndqjacndqja <cndqjacndqja@gmail.com>
2021-04-18 11:37:38 +0900

    BOJ_1916

---
+++ BOJ/다익스트라/BOJ_1916.py
@@ -0,0 +1,35 @@
+from heapq import heappush, heappop
+import sys
+
+n = int(input())
+m = int(input())
+data = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b, c = map(int, sys.stdin.readline().split())
+    data[a].append((b, c))
+
+start, end = map(int, input().split())
+INF = int(1e9)
+visited = [INF for _ in range(n + 1)]
+
+
+def dijkstra():
+    q = []
+    q.append((0, start))
+    visited[start] = 0
+
+    while q:
+        dis, node = heappop(q)
+
+        if visited[node] < dis:
+            continue
+        for i in data[node]:
+            cost = visited[node] + i[1]
+            if cost < visited[i[0]] + dis:
+                visited[i[0]] = cost
+                heappush(q, (cost, i[0]))
+
+    print(visited[end])
+
+
+dijkstra()
