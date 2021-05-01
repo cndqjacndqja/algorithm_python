4690c49e188c592e6b4e6c0612d27fba7695d8cf
cndqjacndqja <cndqjacndqja@gmail.com>
2021-05-01 12:47:25 +0900

    resolved BOJ_1005

--- BOJ/위상 정렬/BOJ_1005.py
+++ BOJ/위상 정렬/BOJ_1005.py
@@ -2,37 +2,29 @@ from _collections import deque
 
 for _ in range(int(input())):
     n, k = map(int, input().split())
-    time = list(map(int, input().split()))
-    data = [[] for _ in range(n+1)]
-    indegree = [0 for _ in range(n+1)]
+    time = [0] + list(map(int, input().split()))
+    data = [[] for _ in range(n + 1)]
+    indegree = [0 for _ in range(n + 1)]
+    dp = [0 for _ in range(n + 1)]
+
     for _ in range(k):
         a, b = map(int, input().split())
         data[a].append(b)
         indegree[b] += 1
+
     q = deque()
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         if indegree[i] == 0:
-            q.append((i, 0))
+            q.append(i)
+            dp[i] = time[i]
 
-    result = [0 for _ in range(n+1)]
-    goal = int(input())
     while q:
-        node, level = q.popleft()
-
-        result[level] = max(result[level], time[node-1])
-        if goal == node:
-            result[level] = time[node-1]
-            break
+        node = q.popleft()
 
         for i in data[node]:
             indegree[i] -= 1
+            dp[i] = max(dp[node] + time[i], dp[i])
             if indegree[i] == 0:
-                q.append((i, level+1))
-
-
-    print(sum(result))
-
-
-
-
-
+                q.append(i)
+    goal = int(input())
+    print(dp[goal])
--- test/test.py
+++ test/test.py
@@ -1,6 +1,30 @@
-str2 = "test"
-str1 = "asdd"
-if str1 < str2:
-    print("dfs")
-else:
-    print("ON")
+from _collections import deque
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    time = [0] + list(map(int, input().split()))
+    data = [[] for _ in range(n + 1)]
+    indegree = [0 for _ in range(n + 1)]
+    dp = [0 for _ in range(n + 1)]
+
+    for _ in range(k):
+        a, b = map(int, input().split())
+        data[a].append(b)
+        indegree[b] += 1
+
+    q = deque()
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+            dp[i] = time[i]
+
+    while q:
+        node = q.popleft()
+
+        for i in data[node]:
+            indegree[i] -= 1
+            dp[i] = max(dp[node] + time[i], dp[i])
+            if indegree[i] == 0:
+                q.append(i)
+    goal = int(input())
+    print(dp[goal])
